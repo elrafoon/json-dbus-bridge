43295a9dbcfc003ca07142f801c73111f1e059f6
Michael Olbrich <m.olbrich@pengutronix.de>
2010-06-01 13:38:05 +0200

    [tests] add copyright header
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- tests/complex_array_test.sh
+++ tests/complex_array_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# json dbus bridge
+#
+# Copyright (c) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# To the extent possible under law, Pengutronix e.K. has waived all copyright
+# and related or neighboring rights to JSON-DBus-Bridge Demo Code.
+#
 
 . `dirname $0`/test_setup.sh "com.pengutronix.jdb.Test.Complex" || exit
 
--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# json dbus bridge
+#
+# Copyright (c) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# To the extent possible under law, Pengutronix e.K. has waived all copyright
+# and related or neighboring rights to JSON-DBus-Bridge Demo Code.
+#
 
 . `dirname $0`/libtest.sh
 
