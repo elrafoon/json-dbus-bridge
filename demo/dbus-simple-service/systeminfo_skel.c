725fef636f9a5e5407f097828a5ca5926a225c12
Michael Olbrich <m.olbrich@pengutronix.de>
2010-06-01 12:49:48 +0200

    [demo] change licence for demo code to CO0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- demo/dbus-simple-service/systeminfo_skel.c
+++ demo/dbus-simple-service/systeminfo_skel.c
@@ -1,20 +1,10 @@
 /*
  * json dbus bridge
  *
- * Copyright (c) 2009 by Michael Olbrich <m.olbrich@pengutronix.de>
+ * Copyright (c) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * To the extent possible under law, Pengutronix e.K. has waived all copyright
+ * and related or neighboring rights to JSON-DBus-Bridge Demo Code.
  */
 
 #include <errno.h>
--- demo/simple-test.sh
+++ demo/simple-test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# json dbus bridge
+#
+# Copyright (c) 2010 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# To the extent possible under law, Pengutronix e.K. has waived all copyright
+# and related or neighboring rights to JSON-DBus-Bridge Demo Code.
+#
 
 url="http://localhost:8080/rpc"
 data='{"service":"com.pengutronix.jdb.simpleinfo|/","method":"com.pengutronix.jdb.SystemInfo.Ping","id":0,"params":["s", "foo"]}'
